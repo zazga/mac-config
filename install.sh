e51468718dddfe50a19497a99863028d2e92df95
zazga <s.vandenhurk@gmail.com>
2025-09-11 17:32:22 +0200

    add yamllint

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 # Master.sh - running all other shell scripts
 echo "Running installers"
 
-for tool in uv alacritty starship fzf fd bat nvim eza zoxide kustomize; do
+for tool in uv alacritty starship fzf fd bat nvim eza zoxide kustomize yamllint; do
   if ! command -v "$tool" &>/dev/null; then
     echo "Installing $tool..."
     "./installers/$tool.sh"
