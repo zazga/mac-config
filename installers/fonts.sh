635d393082b97b1c4f146f5dfca6224dea98fdd4
zazga <s.vandenhurk@gmail.com>
2026-02-12 10:31:37 +0100

    updated font installer to check for installation before installing

--- installers/fonts.sh
+++ installers/fonts.sh
@@ -1,1 +1,11 @@
+#!/bin/bash
+
+if brew list font-caskaydia-mono-nerd-font &>/dev/null; then
+  echo "font-caskaydia-mono-nerd-font is already installed"
+  exit 0
+fi
+
+echo "Installing yq"
+
+# Install fonts via Homebrew
 brew install font-caskaydia-mono-nerd-font
